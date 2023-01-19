48c5efbc71cf94956f5a20912012b672e9a3ac86
matomugo60 <matomugo60@gmail.com>
2023-01-19 18:50:25 +0300

    used useEffect to fetch data

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
+// importing useEffect and useState from react
+import { useEffect, useState } from "react";
 
 
 function MyApp(){
+    // creating variables
 
+     const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+// using useEffect to fetch data
+    useEffect(() =>{
+        setLoading(true);
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            setData(data);
+            setLoading(false);
+        })
+    })
 }
 
 
